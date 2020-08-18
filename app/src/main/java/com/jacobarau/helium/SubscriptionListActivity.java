aa2a21d7c8f25e64f89d1f50849781e140fe2f2c
Jacob Rau <jarau@mtu.edu>
2020-08-17 22:01:09 -0400

    Empty app

---
+++ app/src/main/java/com/jacobarau/helium/SubscriptionListActivity.java
@@ -0,0 +1,13 @@
+package com.jacobarau.helium;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubscriptionListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription_list);
+    }
+}
