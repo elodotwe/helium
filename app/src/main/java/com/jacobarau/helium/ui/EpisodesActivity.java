61abc0cdd08cdeaba894d2833deee6b22f64cdf6
Jacob Rau <jarau@mtu.edu>
2020-08-22 09:15:07 -0400

    Episode list works now, a bit hacky but hey

--- app/src/main/java/com/jacobarau/helium/ui/EpisodesActivity.java
+++ app/src/main/java/com/jacobarau/helium/ui/EpisodesActivity.java
@@ -2,22 +2,91 @@ package com.jacobarau.helium.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.jacobarau.helium.HeliumApplication;
 import com.jacobarau.helium.R;
+import com.jacobarau.helium.jdata.JDataListListener;
 import com.jacobarau.helium.model.Item;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EpisodesActivity extends Activity {
+
+    private SubscriptionListViewModel viewModel;
+    private EpisodeAdapter episodeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episodes);
-        Long id = savedInstanceState.getLong(SubscriptionsActivity.SUBSCRIPTION_ID);
+        Long id = getIntent().getExtras().getLong(SubscriptionsActivity.SUBSCRIPTION_ID);
+        episodeAdapter = new EpisodeAdapter();
+        viewModel = HeliumApplication.wiring.provideSubscriptionListViewModel();
+        viewModel.subscribeToItemsForSubscription(id, episodeAdapter);
+        ListView listView = findViewById(R.id.episode_list);
+        listView.setAdapter(episodeAdapter);
     }
 
-    public void onEpisodesListChanged(List<Item> items) {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        viewModel.unsubscribeItemListener(episodeAdapter);
+    }
 
+    private class EpisodeAdapter extends BaseAdapter implements JDataListListener<Item> {
+        private List<Item> items = new ArrayList<>();
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                convertView = inflater.inflate(R.layout.podcasts_listview_item, parent, false);
+            }
+            TextView title = convertView.findViewById(R.id.podcast_title);
+            title.setText(items.get(position).title);
+            TextView detail = convertView.findViewById(R.id.podcast_summary);
+            DateFormat dateFormat = DateFormat.getDateTimeInstance();
+            detail.setText(dateFormat.format(items.get(position).publishDate));
+
+            return convertView;
+        }
+
+        @Override
+        public void onAdd(Item newElement, int index) {
+
+        }
+
+        @Override
+        public void onDelete(Item deletedElement, int index) {
+
+        }
+
+        @Override
+        public void onDataUpdated(List<Item> value) {
+            this.items = value;
+            notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/jacobarau/helium/ui/SubscriptionListViewModel.java
+++ app/src/main/java/com/jacobarau/helium/ui/SubscriptionListViewModel.java
@@ -4,16 +4,20 @@ import com.jacobarau.helium.HeliumApplication;
 import com.jacobarau.helium.jdata.JDataList;
 import com.jacobarau.helium.jdata.JDataListListener;
 import com.jacobarau.helium.db.PodcastRepository;
+import com.jacobarau.helium.model.Item;
 import com.jacobarau.helium.model.Subscription;
 import com.jacobarau.helium.update.UpdateService;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class SubscriptionListViewModel {
     public final JDataList<Subscription> subscriptions;
     private PodcastRepository podcastRepository;
+    private JDataListListener<Item> itemListener;
 
     public SubscriptionListViewModel(PodcastRepository podcastRepository) {
         subscriptions = new JDataList<>();
@@ -58,5 +62,51 @@ public class SubscriptionListViewModel {
 
     public void updatePodcasts() {
         UpdateService.startUpdate(HeliumApplication.wiring.appContext);
+    }
+
+    public void subscribeToItemsForSubscription(Subscription subscription, JDataListListener<Item> listener) {
+        subscribeToItemsForSubscription(subscription.id, listener);
+    }
+
+    public void subscribeToItemsForSubscription(final long subscriptionID, final JDataListListener<Item> listener) {
+        if (itemListener != null) {
+            throw new RuntimeException("Only one item listener permitted at a time right now");
+        }
+        itemListener = new JDataListListener<Item>() {
+            @Override
+            public void onAdd(Item newElement, int index) {
+                //TODO lol
+            }
+
+            @Override
+            public void onDelete(Item deletedElement, int index) {
+                //TODO why did i even write this list thingy
+            }
+
+            @Override
+            public void onDataUpdated(List<Item> value) {
+                List<Item> filtered = new ArrayList<>(value.size());
+                for (Item item: value) {
+                    if (item.subscriptionId.equals(subscriptionID)) {
+                        filtered.add(item);
+                    }
+                }
+
+                Collections.sort(filtered, new Comparator<Item>() {
+                    @Override
+                    public int compare(Item o1, Item o2) {
+                        return o2.publishDate.compareTo(o1.publishDate);
+                    }
+                });
+
+                listener.onDataUpdated(filtered);
+            }
+        };
+        podcastRepository.items.subscribe(itemListener);
+    }
+
+    public void unsubscribeItemListener(JDataListListener<Item> listener) {
+        podcastRepository.items.unsubscribe(listener);
+        this.itemListener = null;
     }
 }
